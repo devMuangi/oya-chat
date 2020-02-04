1e28d0ebddd914f395c1933a613731cedf359e6a
devMuangi <devmuangi@gmail.com>
2020-02-04 16:51:01 +0300

    Update Chat.js

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = ' https://oya-chat.herokuapp.com/';
+  const ENDPOINT = 'https://oya-chat.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
